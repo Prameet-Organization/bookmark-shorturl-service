814c8a16b77167dd20f7e8a55f73d29d077fc37d
prameet <prameet@prameet-HP-Notebook>
2020-09-05 15:28:20 +0530

    initial commit

---
+++ src/main/java/com/hackathon/bookmarkshorturlservice/BookmarkShorturlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bookmarkshorturlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkShorturlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkShorturlServiceApplication.class, args);
+	}
+
+}
